b958a05c767ebf30ded96938b5ae30f254fd4a5f
Alex <yaroshenko.sashko@gmail.com>
2022-08-16 20:22:03 +0300

    deleted gitignore files

--- keys/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    secret: 'kingstongLoh',
-    link: "mongodb+srv://ALex:7170@workworld.6dz4g.mongodb.net/?retryWrites=true&w=majority",
-    host: 'smtp.ethereal.email',
-    emailPort: 587,
-    ofEmail: "jacynthe91@ethereal.email",
-    passwordOfEmail: "1PETa1UDehJdchvBcu"
-}
